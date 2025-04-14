f7d5eb5fa15d44d39b4ce863aa5c4896b2e06341
jaden-atienza <144278909+jaden-atienza@users.noreply.github.com>
2025-04-14 04:28:18 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
 module.exports = {
     reactStrictMode: true
 }
+
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  basePath: isProd ? '/Website' : '',
+  assetPrefix: isProd ? '/Website/' : '',
+}
