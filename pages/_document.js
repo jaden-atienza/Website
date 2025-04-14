c8786fe03dab7ccde6aa549c35399c6725cc2b27
jaden-atienza <144278909+jaden-atienza@users.noreply.github.com>
2025-04-14 05:39:00 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,16 +3,21 @@ import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 import theme from '../lib/theme'
 
 export default class Document extends NextDocument {
-    render () {
-        return(
-            <Html lang = "en">
-                <Head />
-                <body>
-                    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        )
-    }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
