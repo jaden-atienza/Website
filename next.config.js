7988e144d56eb15e9cc93ca912097a3640bff81f
jaden-atienza <144278909+jaden-atienza@users.noreply.github.com>
2025-04-14 04:41:33 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
-    reactStrictMode: true
+  reactStrictMode: true, // ✅ safe and recommended
+  basePath: isProd ? '/Website' : '',
+  assetPrefix: isProd ? '/Website/' : '',
+  trailingSlash: true, // ✅ needed for GitHub Pages
 }
