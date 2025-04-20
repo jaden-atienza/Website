11293aa04b48da87ecc502dafd7f47158c867478
jaden-atienza <144278909+jaden-atienza@users.noreply.github.com>
2025-04-20 03:49:59 -0400

    Update voxel-penguin.js

--- components/voxel-penguin.js
+++ components/voxel-penguin.js
@@ -131,7 +131,7 @@ const VoxelPenguin = () => {
         ref={refContainer}
         className='voxel-penguin' 
         m="auto" 
-        at={['-20px', '-60px', '-120px']}
+        mt={['-20px', '-60px', '-120px']}
         mb={['-40px', '-140px', '-200px']} 
         w={[280, 480, 640]}
         h={[280, 480,640]}
