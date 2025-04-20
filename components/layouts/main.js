112e77f465efbd391b83948339194fdaf79c0ca4
jaden-atienza <144278909+jaden-atienza@users.noreply.github.com>
2025-04-20 03:26:38 -0400

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Navbar from '../navbar.js'
-import { Box, Container } from '@chakra-ui/react' 
+import NoSsr from '../no-ssr.js'
+import { Box, Container } from '@chakra-ui/react'
+import VoxelPenguin from '../voxel-penguin.js''
 
 const Main = ({ children, router}) => {
     return (
@@ -13,6 +15,9 @@ const Main = ({ children, router}) => {
             <Navbar path={router.asPath} />
 
             <Container maxW="container.md" pt={14}>
+                <NoSsr>
+                <VoxelPenguin />
+                </NoSsr>
                 {children}
             </Container>
         </Box>
