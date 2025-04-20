5cb8ca2daa173093fd69046511af21ce970f55f7
jaden-atienza <144278909+jaden-atienza@users.noreply.github.com>
2025-04-20 03:58:55 -0400

    Update voxel-penguin.js

--- components/voxel-penguin.js
+++ components/voxel-penguin.js
@@ -3,7 +3,6 @@ import { Box, Spinner } from '@chakra-ui/react'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { LoadGLTFModel } from '../lib/model'
-import { animate } from 'framer-motion'
 
 function easeOutCirc(x) {
     return Math.sqrt(1 - Math.pow(x - 1, 4))
