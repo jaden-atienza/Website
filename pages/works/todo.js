386b171d6dd4c328b07049319c72a2592ff91155
Jaden Atienza <144278909+jaden-atienza@users.noreply.github.com>
2025-04-20 23:26:14 -0400

    Update todo.js

--- pages/works/todo.js
+++ pages/works/todo.js
@@ -10,20 +10,27 @@ import { Title, WorkImage, Meta } from '../../components/work'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
 
-const Work = () => {
-    return (
+const Work = () => (
         <Layout title="Todo List">
             <Container>
                 <Title>
                     Todo <Badge>2025</Badge>
                 </Title>
                 <P>
-                A simple full-stack to-do list application built with React (Vite) on the frontend and Node.js + Express + LowDB on the backend. Users can add, complete, delete, and filter tasks (All / Active / Completed). Tasks are stored persistently using a lightweight JSON database, making it perfect for small projects or quick demos.        
+                A simple full-stack to-do list application built with React (Vite) on the frontend and 
+                Node.js + Express + LowDB on the backend. Users can add, complete, delete, and 
+                filter tasks (All / Active / Completed). Tasks are stored persistently using a lightweight JSON database, 
+                making it perfect for small projects or quick demos.        
                 </P>
                 <List ml={4} my={4}></List>
+                    <ListItem>
+                    <Meta>Website</Meta>
+                    <Link href="https://github.com/jaden-atienza/Todo">
+                        https://github.com/jaden-atienza/Todo <ExternalLinkIcon mx="2px" />
+                    </Link>
+                    </ListItem>
             </Container>
         </Layout>
-    )
-}
+)
 
 export default Work
