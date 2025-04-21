6e8c4f8a68ddb45300bd1a2818efac9f3b971548
Jaden Atienza <144278909+jaden-atienza@users.noreply.github.com>
2025-04-20 21:36:00 -0400

    Update works.js

--- pages/works.js
+++ pages/works.js
@@ -22,8 +22,8 @@ const Works = () => {
             <SimpleGrid colum={[1,1,2]} gap={6}>
                 <Section>
                     <WorkGridItem 
-                    id="ToDo List" 
-                    title='Project ToDo List' 
+                    id="Todo List" 
+                    title='ToDo List' 
                     thumbnail={todo} 
                     >
                     A simple full-stack to-do list application built with React (Vite) on the front end and Node.js + Express + LowDB on the backend. Users can add, complete, delete, and filter tasks (All / Active / Completed). Tasks are stored persistently using a lightweight JSON database, making it perfect for small projects or quick demos.
