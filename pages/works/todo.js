eabe31d23a1b58c0aee927c0360ce59f5e7247b2
Jaden Atienza <144278909+jaden-atienza@users.noreply.github.com>
2025-04-21 00:45:10 -0400

    Update todo.js

--- pages/works/todo.js
+++ pages/works/todo.js
@@ -1,79 +1,39 @@
-import {
+import { 
     Container,
-    Heading,
-    SimpleGrid,
-    Divider
-} from '@chakra-ui/react'
-import Section from '../components/section'
-import { WorkGridItem } from '../components/grid-item'
-import Layout from '../components/layouts/article'
+    Badge,
+    Link,
+    List,
+    ListItem 
+} from "@chakra-ui/react"
+import { ExternalLinkIcon } from "@chakra-ui/icons"
+import { Title, WorkImage, Meta } from '../../components/works'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
 
-import thumbUtcc from '../public/images/works/thumbUtcc.png'
-import thumbShirt from '../public/images/works/thumbShirt.png'
-import todo from '../public/images/works/todo.png'
-import mystery from '../public/images/works/mystery.png'
+const Work = () => {
+    return (
+        <Layout title="Todo List">
+            <Container>
+                <Title>
+                    Todo <Badge>2025</Badge>
+                </Title>
+                <P>
+                A simple full-stack to-do list application built with React (Vite) on the frontend and 
+                Node.js + Express + LowDB on the backend. Users can add, complete, delete, and 
+                filter tasks (All / Active / Completed). Tasks are stored persistently using a lightweight JSON database, 
+                making it perfect for small projects or quick demos.        
+                </P>
+                <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>Website</Meta>
+                            <Link href="https://github.com/jaden-atienza/Todo">
+                        https://github.com/jaden-atienza/Todo <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
+                </List>
+            </Container>
+        </Layout>
+    )
+}
 
-const Works = () => (
-    <Layout title="Works">
-        <Container>
-            <Heading as="h3" fontSize={20} mb={4}>
-                Works
-            </Heading>
-
-            <SimpleGrid colum={[1,1,2]} gap={6}>
-                <Section>
-                    <WorkGridItem 
-                    id="Todo List" 
-                    title='ToDo List' 
-                    thumbnail={todo} 
-                    >
-                    A simple full-stack to-do list application built with React (Vite) on the front end and Node.js + Express + LowDB on the backend. Users can add, complete, delete, and filter tasks (All / Active / Completed). Tasks are stored persistently using a lightweight JSON database, making it perfect for small projects or quick demos.
-                    </WorkGridItem>
-                </Section>
-
-                <Section>
-                    <WorkGridItem 
-                    id="Mystery" 
-                    title='Mystery' 
-                    thumbnail={mystery} 
-                    >
-                    Mystery Game Project ** Work In Progress **
-                    </WorkGridItem>
-                </Section>
-                <Section delay={0.1}>
-
-                </Section>
-
-                <Section delay={0.2}>
-                    <Divider my={6} />
-
-                    <Heading as="h3" fontSize={20} mb={4}>
-                        Design
-                    </Heading>
-                </Section>
-
-                <Section>
-                    <WorkGridItem 
-                    id="UTCC Brand" 
-                    title='UTCC Branding Lead' 
-                    thumbnail={thumbUtcc} 
-                    >
-                    Developed and executed the visual branding strategy for the Univeristy of Toronto Climbing Club, including logo design, social media aesthetics, and promotional content.
-                    </WorkGridItem>
-                </Section>
-                
-                <Section>
-                    <WorkGridItem 
-                    id="T-shirt Design" 
-                    title="UTCC T-shirt Design" 
-                    thumbnail={thumbShirt}
-                    >
-                    Created custom shirt designs that reflect the idenity and values of UTCC, focusing on clean visuals, colour coordination, and wearability.
-                    </WorkGridItem>
-                </Section>
-            </SimpleGrid>
-        </Container>
-    </Layout>
-)
-
-export default Works
+export default Work
