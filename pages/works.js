684ca8359c6693ef1c4ff255895b881f52a0d9b9
Jaden Atienza <144278909+jaden-atienza@users.noreply.github.com>
2025-04-20 23:55:00 -0400

    Update works.js

--- pages/works.js
+++ pages/works.js
@@ -7,6 +7,7 @@ import {
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
 import Layout from '../components/layouts/article'
+
 import thumbUtcc from '../public/images/works/thumbUtcc.png'
 import thumbShirt from '../public/images/works/thumbShirt.png'
 import todo from '../public/images/works/todo.png'
@@ -14,10 +15,10 @@ import todo from '../public/images/works/todo.png'
 const Works = () => {
     return (
         <Layout>
-        <Container>
-            <Heading as="h3" fontSize={20} mb={4}>
+            <Container>
+                <Heading as="h3" fontSize={20} mb={4}>
                 Works
-            </Heading>
+                </Heading>
 
             <SimpleGrid colum={[1,1,2]} gap={6}>
                 <Section>
@@ -28,6 +29,9 @@ const Works = () => {
                     >
                     A simple full-stack to-do list application built with React (Vite) on the front end and Node.js + Express + LowDB on the backend. Users can add, complete, delete, and filter tasks (All / Active / Completed). Tasks are stored persistently using a lightweight JSON database, making it perfect for small projects or quick demos.
                     </WorkGridItem>
+                </Section>
+
+                <Section>
                     <WorkGridItem 
                     id="UTCC Brand" 
                     title='UTCC Branding Lead' 
@@ -35,6 +39,9 @@ const Works = () => {
                     >
                     Developed and executed the visual branding strategy for the Univeristy of Toronto Climbing Club, including logo design, social media aesthetics, and promotional content.
                     </WorkGridItem>
+                </Section>
+                
+                <Section>
                     <WorkGridItem 
                     id="T-shirt Design" 
                     title="UTCC T-shirt Design" 
