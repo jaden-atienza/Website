210e73975ea2405053da7614b48de85f8cf4c8cd
Jaden Atienza <144278909+jaden-atienza@users.noreply.github.com>
2025-04-20 21:42:55 -0400

    Update todo.js

--- pages/works/todo.js
+++ pages/works/todo.js
@@ -20,6 +20,7 @@ const Work = () => {
                 <P>
                 A simple full-stack to-do list application built with React (Vite) on the frontend and Node.js + Express + LowDB on the backend. Users can add, complete, delete, and filter tasks (All / Active / Completed). Tasks are stored persistently using a lightweight JSON database, making it perfect for small projects or quick demos.        
                 </P>
+                <List ml={4} my={4}></List>
             </Container>
         </Layout>
     )
