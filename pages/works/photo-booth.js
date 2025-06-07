f702cfc4e174427b7a2c806a64d10f30741bd3e6
Jaden Atienza <144278909+jaden-atienza@users.noreply.github.com>
2025-06-07 17:55:55 -0400

    Create photo-booth.js

---
+++ pages/works/photo-booth.js
@@ -0,0 +1,44 @@
+import { 
+    Container,
+    Badge,
+    Link,
+    List,
+    ListItem 
+} from "@chakra-ui/react"
+import { ExternalLinkIcon } from "@chakra-ui/icons"
+import { Title, WorkImage, Meta } from '../../components/works'
+import P from '../../components/paragraph'
+import Layout from '../../components/layouts/article'
+
+const Work = () => {
+    return (
+        <Layout title="Bunluke's Photo Booth">
+            <Container>
+                <Title>
+                    ToDo List<Badge>2025</Badge>
+                </Title>
+                <P>
+                Designed a multi-layout photo booth web app that captures webcam images, applies countdown timers, and generates downloadable photo strips.        
+                </P>
+                <List ml={4} my={4}>
+                    <ListItem>
+                        <Meta>Website</Meta>
+                            <Link href="https://github.com/jaden-atienza/photo-booth">
+                        https://github.com/jaden-atienza/photo-booth <ExternalLinkIcon mx="2px" />
+                        </Link>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Platform</Meta>
+                        <span>Web</span>
+                    </ListItem>
+                    <ListItem>
+                        <Meta>Stack</Meta>
+                        <span>React, JavaScript, HTML2Canvas</span>
+                    </ListItem>
+                </List>
+            </Container>
+        </Layout>
+    )
+}
+
+export default Work
