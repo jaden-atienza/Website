ec0a1de35be03217dbe3e1a0b2cda2ea5a75d9c0
Jaden Atienza <144278909+jaden-atienza@users.noreply.github.com>
2025-06-10 18:13:42 -0400

    Update photobooth.js

--- pages/works/photobooth.js
+++ pages/works/photobooth.js
@@ -23,7 +23,7 @@ const Work = () => {
                 <List ml={4} my={4}>
                     <ListItem>
                         <Meta>Website</Meta>
-                            <Link href="https://github.com/jaden-atienza/photo-booth">
+                            <Link href="https://photo-booth-5b31r369u-jaden-atienzas-projects.vercel.app">
                         https://github.com/jaden-atienza/photo-booth <ExternalLinkIcon mx="2px" />
                         </Link>
                     </ListItem>
